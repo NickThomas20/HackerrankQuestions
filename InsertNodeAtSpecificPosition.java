c1cdfd7f70b00d873e81ddebcbca53655e1ce713
Nick Thomas <73144044+NickThomas20@users.noreply.github.com>
2022-09-26 20:28:59 -0500

    Update InsertNodeAtSpecificPosition.java

--- InsertNodeAtSpecificPosition.java
+++ InsertNodeAtSpecificPosition.java
@@ -19,3 +19,32 @@
     }
 
 }
+
+// Write a function that inserts a new item into a Linked List of integers at the given position.
+
+// Input Format
+
+// Your function should accept the following parameters:
+
+// Parameter #1: The first Node of the Linked List (Node is defined in the sample code below)
+// Parameter #2: The integer value to be inserted
+// Parameter #3: The zero-indexed integer position to insert the item
+// Constraints
+
+// 1 <= n <= 1000
+// Insertion index will be in the range [0, n]
+// Output Format
+
+// Your function should return the first Node of the Linked List with the new value inserted.
+
+// Sample Input 0
+
+// 3
+// 1
+// 3
+// 5
+// 2
+// 1
+// Sample Output 0
+
+// 1,2,3,5
