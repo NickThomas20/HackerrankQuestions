3361ee825dfa0291555e17ad56d75b96fa1b7c9f
Nick Thomas <73144044+NickThomas20@users.noreply.github.com>
2022-10-14 12:26:46 -0500

    Create RemoveDupFromListNode.java

---
+++ RemoveDupFromListNode.java
@@ -0,0 +1,31 @@
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode list = head;
+
+         while(list != null){
+             if(list.next == null){
+                 break;
+             }
+             if(list.val == list.next.val){
+                 list.next = list.next.next; 
+             }
+             //move to the next
+             else{
+                 list = list.next;
+             }
+         }
+     
+        return head;
+    }
+}
